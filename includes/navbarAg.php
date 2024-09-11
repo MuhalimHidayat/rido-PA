6c857295e87e66df0bd3503d070dd930ff89d44e
MuhalimHidayat <hidayatalim27@gmail.com>
2024-09-11 13:54:40 +0700

    navbar siswa perubahan path pada profile

--- includes/navbarAg.php
+++ includes/navbarAg.php
@@ -5,69 +5,6 @@
 ?>
 <header>
     <div id="navbar">
-        <!-- <div class="flex justify-between items-center bg-gray-800 text-gray-400 px-5" id="navbar-top">
-            <div class="navbar-top-content flex gap-3">
-                <a class="focus" href="">Home</a>
-                <a href="">Courses</a>
-                <a href="">About</a>
-                <a href="">Contact</a>
-                <a href="">Become an Instructor</a>
-            </div>
-            <div class="flex">
-                
-                <div>
-
-                    <button id="currencyDropdownButton" data-dropdown-toggle="currencyDropdown" data-dropdown-placement="bottom" class="me-3 mb-3 md:mb-0 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center " type="button">USD <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-                        </svg>
-                    </button>
-
-                    <div id="currencyDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="currencyDropdownButton">
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                </div>
-
-                <div>
-                    <button id="languangeDropdownButton" data-dropdown-toggle="languangeDropdown" data-dropdown-placement="bottom" class="me-3 mb-3 md:mb-0 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center " type="button">English <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-                        </svg>
-                    </button>
-
-                    <div id="languangeDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="languangeDropdownButton">
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                </div>
-            </div>
-
-        </div> -->
         <div class="" id="navbar-bottom">
             <div class="navbar-bottom-content flex flex-row justify-between items-center px-5">
                 <div class="flex flex-row items-center gap-5 py-4 px-5">
@@ -85,7 +22,7 @@
                         $agent = query("SELECT * FROM agent WHERE id_agent = $id_agent")[0];    
                         ?>
                         <div class="navbar-bottom-content-img me-8 my-3">
-                            <a href="viewAgent.php">
+                            <a href="settingAg.php">
                                 <img src="assets/fotoUploads/<?= $agent['foto'] ?>" alt="">
                             </a>
                         </div>
